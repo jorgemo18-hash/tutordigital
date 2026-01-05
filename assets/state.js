214b579bfe767c802855f4cdabd01a98a8589e9e
jorgemo18-hash <jorgemo18@gmail.com>
2026-01-05 14:43:31 +0100

    Update state.js

--- assets/state.js
+++ assets/state.js
@@ -7,6 +7,11 @@ export const DOM = {
   kbd: document.getElementById("kbd"),
   pad: document.getElementById("pad"),
   eqPreview: document.getElementById("eqPreview"),
+  agenda: document.getElementById("agenda"),
+initialRow: document.getElementById("initialRow"),
+btnDeberes: document.getElementById("btnDeberes"),
+btnExamen: document.getElementById("btnExamen"),
+btnTrabajo: document.getElementById("btnTrabajo"),
 
   micBtn: document.getElementById("mic"),
   // si no existen en tu HTML, que queden en null sin romper nada
