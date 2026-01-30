46a9de2498d5e46b29794045bdbc5d30ee8ac3f6
jorgemo18-hash <jorgemo18@gmail.com>
2026-01-30 13:18:42 +0100

    alpha v2.0.18

--- assets/student/student.js
+++ assets/student/student.js
@@ -308,8 +308,6 @@ function downloadFile(file) {
   const link = document.createElement("a");
   link.href = url;
   link.download = file.name || "adjunto";
-  link.target = "_blank";
-  link.rel = "noopener";
   document.body.appendChild(link);
   link.click();
   link.remove();
